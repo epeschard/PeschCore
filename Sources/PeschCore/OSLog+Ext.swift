8fcd092cb085ef3b14e39feca24d70caa05b4ad4
Eugene Peschard <e.peschard@scrm.eu>
2019-10-14 08:25:47 +0200

    updated os_log categories

--- Sources/PeschCore/OSLog+Ext.swift
+++ Sources/PeschCore/OSLog+Ext.swift
@@ -13,16 +13,17 @@ extension OSLog {
 
     private static var mainBundleId = Bundle.main.bundleIdentifier!
 
-    static let helper = OSLog(subsystem: mainBundleId,
-                              category: "helper")
-    static let library = OSLog(subsystem: mainBundleId,
-                               category: "library")
-    static let common = OSLog(subsystem: mainBundleId,
-                              category: "common")
-    static let delegate = OSLog(subsystem: mainBundleId,
-                                category: "delegate")
+    static let viewCycle = OSLog(subsystem: mainBundleId,
+                                 category: "viewCycle")
+    static let view = OSLog(subsystem: mainBundleId,
+                            category: "view")
+    static let interactor = OSLog(subsystem: mainBundleId,
+                                  category: "interactor")
+    static let presenter = OSLog(subsystem: mainBundleId,
+                                 category: "presenter")
+    static let entity = OSLog(subsystem: mainBundleId,
+                              category: "entity")
     static let repo = OSLog(subsystem: mainBundleId,
                             category: "repo")
-    static let scene = OSLog(subsystem: mainBundleId,
-                              category: "scene")
+
 }
